ab70e02ec99c1fb7fc56735d610c12c9bcc517a5
Vegetal <Vegetal>
2025-05-23 02:17:54 -0500

    test 2

--- PulgarVerde/Cliente/VegetalHTTPCliente.swift
+++ PulgarVerde/Cliente/VegetalHTTPCliente.swift
@@ -10,7 +10,7 @@ import Foundation
 struct VegetalHTTPCliente {
     
     func fetchVegetales() async throws -> [Vegetal] {
-        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://raw.githubusercontent.com/CrisQF/Vegetales/refs/heads/main/Vegetales.json")!)
+        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://raw.githubusercontent.com/CrisQF/PulgarVerde/refs/heads/main/Vegetales.json")!)
         return try JSONDecoder().decode([Vegetal].self, from: data)
     }
 }
